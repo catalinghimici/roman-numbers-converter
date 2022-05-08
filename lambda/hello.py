bc8cbadb265a1dfecb09401763bb1305bcd5417f
catalinghimici <catalin.ghimici@outlook.com>
2022-05-08 23:40:28 +0300

    Added unit tests for roman numbers conversion

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,3 +1,12 @@
+class InvalidRomanNumeralError(Exception):
+    pass
+
 
 def handler(event, context):
     print('hello')
+
+
+def roman_to_int(roman_number: str) -> int:
+    pass
+
+
---
+++ test/roman_numbers_converter_tests.py
@@ -0,0 +1,61 @@
+import unittest
+from hello import roman_to_int, InvalidRomanNumeralError
+
+
+class FromRomanBadInput(unittest.TestCase):
+    def test_no_input(self):
+        """
+        Conversion should fail when no input is provided
+        """
+        self.assertRaises(InvalidRomanNumeralError, roman_to_int, None)
+
+    def test_input_is_not_string(self):
+        """
+        Conversion should fail when no input is not the correct format
+        """
+        for roman_number in ([], 1234, {'MC'}):
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number)
+
+    def test_contains_wrong_characters(self):
+        """
+        Conversion should fail with strings that contain characters that are not part of roman numerals
+        Allowed characters: I, V, X, L, D, C, M
+        """
+        for roman_number in ('M-', 'DIa', '1234', 'asd'):
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number)
+
+    def test_too_many_repeated_numerals(self):
+        """
+        Conversion should fail with too many repeated numerals
+        M, C, X, I can repeat 3 times
+        D, L, V can only appear once
+        """
+        for roman_number in ('MMMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number)
+
+    def test_repeated_pairs(self):
+        """
+        Conversion should fail with repeated pairs of numerals
+        """
+        for roman_number in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number)
+
+    def test_malformed_antecedent(self):
+        """
+        Conversion should fail with malformed antecedents
+        """
+        for roman_number in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV',
+                             'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number)
+
+    def test_from_roman_case(self):
+        """
+        Conversion should only accept uppercase input
+        """
+        for roman_number in ('mmc', 'viii'):
+            roman_to_int(roman_number.upper())
+            self.assertRaises(InvalidRomanNumeralError, roman_to_int, roman_number.lower())
+
+
+if __name__ == '__main__':
+    unittest.main()
